adcd59983436f6d38b25b500758a57afe01898d0
Chianh1303 <chianhn567@gmai.com>
2024-12-04 15:04:43 +0700

    hoan thanh bai 5

---
+++ src/main/java/org/example/demo9/CinimaManagement.java
@@ -0,0 +1,57 @@
+package org.example.demo9;
+
+public class CinimaManagement {
+    private int idTicket;
+    private String nameMovie;
+    private String Showtime;
+    private String TicketPrice;
+
+    public CinimaManagement(int idTicket, String nameMovie, String showtime, String ticketPrice) {
+        this.idTicket = idTicket;
+        this.nameMovie = nameMovie;
+        Showtime = showtime;
+        TicketPrice = ticketPrice;
+    }
+
+    public int getIdTicket() {
+        return idTicket;
+    }
+
+    public void setIdTicket(int idTicket) {
+        this.idTicket = idTicket;
+    }
+
+    public String getNameMovie() {
+        return nameMovie;
+    }
+
+    public void setNameMovie(String nameMovie) {
+        this.nameMovie = nameMovie;
+    }
+
+    public String getShowtime() {
+        return Showtime;
+    }
+
+    public void setShowtime(String showtime) {
+        Showtime = showtime;
+    }
+
+    public String getTicketPrice() {
+        return TicketPrice;
+    }
+
+    public void setTicketPrice(String ticketPrice) {
+        TicketPrice = ticketPrice;
+    }
+
+    @Override
+    public String toString() {
+        return "CinimaManagement{" +
+                "idTicket='" + idTicket + '\'' +
+                ", nameMovie='" + nameMovie + '\'' +
+                ", Showtime='" + Showtime + '\'' +
+                ", TicketPrice='" + TicketPrice + '\'' +
+                '}';
+    }
+}
